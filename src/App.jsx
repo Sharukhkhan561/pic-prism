d24191b9b799dd2e3e151245080edd42c4bae9b5
Sharukh Khan <enetsharukh@gmail.com>
2024-08-01 13:27:44 +0530

    Navbar UI done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import './App.css'
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login"
 import SellerDashboard from "./pages/SellerDashboard";
 import BuyerDashboard from "./pages/BuyerDashboard";
+import Navbar from './components/Navbar';
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-col sm:flex-row justify-between items-start sm:items-center px-5 py-5 fixed
+     top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30 bg-white">
+      {/* logo and site name  */}
+
+      <div className="flex justify-between items-center">
+        {/* add image  */}
+        <Link to="/" className="font-bold text-3xl">
+          Pic prism
+        </Link>
+      </div>
+      <ul className="flex gap-5 text-lg font-semibold text-gray-400">
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">About</Link>
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">Contact</Link>
+        <Link to="/login" className="hover:text-black cursor-pointer sm:p-2">Log in</Link>
+        <Link to="/signup" className="hover:text-black cursor-pointer sm:p-2">Sign up</Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
